db422e88874171aba26be38c0d5001abc85b1b90
Mikey Lee <124496222+relaxmikeydoesit@users.noreply.github.com>
2023-11-12 10:11:54 -0500

    Added a navbar to design.

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from '@/styles/Navbar.module.css';
+import Link from 'next/link';
+
+export default function Navbar() {
+    return (
+    <>
+    <div className={styles.navbar}>
+    <Link href='/'>Home</Link>
+  <Link href='/about'>About</Link>
+  <Link href='/contact'>Contact</Link>
+  <Link href='/help'>Support</Link>
+    </div>
+    </>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from '@/styles/Home.module.css';
+import Navbar from '@/components/Navbar';
+import Link from 'next/link';
 import Hero from '@/components/Hero';
 import UtilityTimer from '@/components/UtilityTimer';
 import LilListr from '@/components/LilListr';
+
 
 export default function Home() {
 
@@ -10,6 +13,12 @@ export default function Home() {
     <>
     <h1 className={styles.bigGoldText}>Hello, Friend!</h1>
       <h2 className={styles.inverseText}>PRODUCTIVITY HUB | Created with React and NEXT.js</h2>
+      <Navbar className={styles.navbar} style={{}}>
+        <Link href='/'>Home ||</Link>
+        <Link href='/'>About || </Link>
+        <Link href='/'>Contact ||</Link>
+        <Link href='/'>Need Help?</Link>
+  </Navbar>
       <Hero heroImage='/hero-flash.jpg'/>
       <UtilityTimer className={styles.timer} />
       <LilListr className={styles.LilListr} />     
