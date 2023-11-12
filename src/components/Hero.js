d8bcab1189d93570571a6a1ea8520a664418facd
Mikey Lee <124496222+relaxmikeydoesit@users.noreply.github.com>
2023-11-12 06:30:59 -0500

    Included a full-height hero image + intro msg.

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '@/styles/Hero.module.css';
+
+export default function Hero(props){
+    // console.log(props);
+    return (
+        <>
+        <div className={styles.hero} style={{backgroundImage: `url(${props.heroImage})`}}></div>
+        </>
+
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from '@/styles/Home.module.css';
+import Hero from '@/components/Hero';
 
 export default function Home() {
 
   return (
     <>
+          <Hero heroImage='/hero-flash.jpg'/>
       <h1 className={styles.bigGoldText}>Hello, friend!</h1>
       <h2 className={styles.inverseText}> This Single Page Application is built with React & Next.js. </h2>
-
+     
+  
     </>
   );
 }
